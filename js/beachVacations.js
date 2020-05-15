fcdba64835bce1ceea54a2ba725601ed21d1af2a
svitlana-new <galka.mospan@gmail.com>
2020-05-15 14:39:40 +0000

    added comments alert

--- js/beachVacations.js
+++ js/beachVacations.js
@@ -7,3 +7,10 @@ function sendBtnPressed() {
   formMail.value = "";
   formText.value = "";
 }
+document.querySelector('.addResponses').addEventListener('click', responce);
+function responce (){
+    for (let i=0; i<2; i++) {
+let resps = prompt('Напишіть своє ім\'я та залиште відгук у наступному вікні');
+document.querySelector('.addedResp').append(resps + ' ' +' ');
+    }
+}
